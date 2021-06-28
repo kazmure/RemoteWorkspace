3d01d0351df8053f77bca057a2f4e7ce0577eada
kazmure <@DESKTOP-3MMK550>
2021-06-28 21:22:58 +0300

    dfdf

--- src/main/java/net/mcreator/test/block/VaviaivaBlock.java
+++ src/main/java/net/mcreator/test/block/VaviaivaBlock.java
@@ -1,17 +1,29 @@
 
 package net.mcreator.test.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.test.TestModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @TestModElements.ModElement.Tag
 public class VaviaivaBlock extends TestModElements.ModElement {
-
 	@ObjectHolder("test:vaviaiva")
 	public static final Block block = null;
-
 	public VaviaivaBlock(TestModElements instance) {
 		super(instance, 4);
-
 	}
 
 	@Override
@@ -20,24 +32,18 @@ public class VaviaivaBlock extends TestModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
 			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
-
 			setRegistryName("vaviaiva");
 		}
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
